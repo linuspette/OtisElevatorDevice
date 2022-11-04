4714b7bcccf583e014c7f598d515c36e763d9a62
Daniel Lenberg <danneeeee@hotmail.com>
2022-11-04 09:51:22 +0100

    fixed errors with program.cs and props

--- OtisElevatorDevice/Program.cs
+++ OtisElevatorDevice/Program.cs
@@ -1,17 +1,28 @@
 ﻿using OtisElevatorDevice.Models;
 using OtisElevatorDevice.Services;
-
+using System.Drawing.Text;
 
 namespace OtisElevatorDevice;
 
 
 class Program
 {
+    DeviceManager deviceManager = new DeviceManager();
+    List<ElevatorListItem> elevatorListItems = new List<ElevatorListItem>();
+
     static void Main()
     {
 
+        Initialize();
+
+        
 
 
+
+
+    }
+    static void Initialize()
+    {
 
 
 
@@ -24,4 +35,3 @@ class Program
 
 
 
->>>>>>>>> Temporary merge branch 2
